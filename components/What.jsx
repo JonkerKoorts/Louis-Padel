91484fc798d0a34e6be385b843580e4e69d76167
Jonker <101875258+JonkerKoorts@users.noreply.github.com>
2023-07-21 08:25:06 +0200

    rest done

--- components/What.jsx
+++ components/What.jsx
@@ -10,6 +10,10 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { useRouter } from "next/navigation";
+import { Player } from "video-react"; // import the video-react components
+import "video-react/dist/video-react.css";
+import Lottie from "lottie-react";
+import Construction from "../public/constructionlottie.json";
 
 const What = () => {
   const router = useRouter();
@@ -55,7 +59,7 @@ const What = () => {
             <h1 className="text-center text-[40px] font-bold mb-20">
               What We Do
             </h1>
-            <div className="gap-4 md:gap-8 flex flex-col md:flex-row justify-between content-center items-center">
+            <div className="gap-4 md:gap-8 flex flex-col md:flex-row justify-between content-center ">
               <div className="flex flex-col w-full md:w-1/2">
                 <motion.h1
                   className="text-[25px] md:text-[40px] font-bold mb-6 bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
@@ -69,98 +73,158 @@ const What = () => {
                 >
                   Padel is one of the fastest growing sports in the world. As a
                   result, there is an increasing demand for high-quality padel
-                  courts. Investing in a padel court not only adds value to your
-                  property but also promotes a healthy and social lifestyle.
+                  courts. Padel Pro is a South African enterprise that takes
+                  immense pride in crafting, producing, and setting up top-tier
+                  padel courts to industry standards. By harnessing locally
+                  sourced materials, Padel Pro presents Padel Solutions, a
+                  comprehensive service that delivers the required expertise for
+                  the installation of personalised court structures.
                 </motion.p>
+                <br className="hide-on-mobile" />
+                <br className="hide-on-mobile" />
+                <div className=" decoration-white">
+                  <Lottie animationData={Construction} />
+                </div>
               </div>
               <div className="w-full md:w-1/2">
-                <Accordion type="single" collapsible className="mt-4 md:mt-0">
-                  <AccordionItem value="item-1">
-                    <AccordionTrigger
-                      onClick={() =>
-                        setActiveItem(activeItem === "item-1" ? null : "item-1")
-                      }
-                    >
-                      <div
-                        className={`font-bold ${
-                          activeItem === "item-1"
-                            ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
-                            : "text-black"
-                        }`}
+                <div>
+                  <div className="text-[25px] md:text-[40px] font-bold mb-6 bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent">
+                    Take a look at our Comprehensive packages.
+                    <p className="text-sm text-black md:text-base font-light mb-4">
+                      Packages are tailored to fit any person looking to invest
+                      form our most basic to most comprehensive, find what is
+                      right for you.
+                      <br />{" "}
+                      <span className="font-extrabold bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent">
+                        Designing with you for you
+                      </span>
+                    </p>
+                  </div>
+                  <Accordion type="single" collapsible className="mt-4 md:mt-0">
+                    <AccordionItem value="item-1">
+                      <AccordionTrigger
+                        onClick={() =>
+                          setActiveItem(
+                            activeItem === "item-1" ? null : "item-1"
+                          )
+                        }
                       >
-                        Expertise in Padel Courts
-                      </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      What Construction prides itself in building world-class
-                      Padel courts with meticulous attention to detail.
-                    </AccordionContent>
-                  </AccordionItem>
-                  <AccordionItem value="item-2">
-                    <AccordionTrigger
-                      onClick={() => setActiveItem("item-2")}
-                      onClose={() => setActiveItem(null)}
-                    >
-                      <div
-                        className={`font-bold ${
-                          activeItem === "item-2"
-                            ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
-                            : "text-black"
-                        }`}
+                        <div
+                          className={`font-bold ${
+                            activeItem === "item-1"
+                              ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
+                              : "text-black"
+                          }`}
+                        >
+                          Outdoor Basic
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        Introducing Mod. BASIC – a straightforward pillar
+                        structure measuring 100 x 50 mm, designed with
+                        cost-effectiveness in mind. Ideal for indoor
+                        installations, this model offers a budget-friendly
+                        option. For enhanced stability, we strongly advise
+                        opting for our frame construction (check out mod. BASIC
+                        PRO / PREMIUM). The frame ensures solid support for the
+                        glass walls on all four sides. Enjoy a hassle-free
+                        experience as the project is supplied and built as a
+                        complete turnkey solution.
+                      </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-2">
+                      <AccordionTrigger
+                        onClick={() => setActiveItem("item-2")}
+                        onClose={() => setActiveItem(null)}
                       >
-                        Quality and Durability
-                      </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      We ensure the best in quality and durability, matching
-                      international standards.
-                    </AccordionContent>
-                  </AccordionItem>
-                  <AccordionItem value="item-3">
-                    <AccordionTrigger
-                      onClick={() => setActiveItem("item-3")}
-                      onClose={() => setActiveItem(null)}
-                    >
-                      <div
-                        className={`font-bold ${
-                          activeItem === "item-3"
-                            ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
-                            : "text-black"
-                        }`}
+                        <div
+                          className={`font-bold ${
+                            activeItem === "item-2"
+                              ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
+                              : "text-black"
+                          }`}
+                        >
+                          Basic Pro
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        Presenting the ultimate Panoramic Padel Court,
+                        unrivalled in the market. This extraordinary court
+                        boasts exceptional structural strength, offering
+                        unparalleled resistance against wind loads. Its
+                        spectacularly robust design ensures an elevated Padel
+                        experience, allowing you to indulge in the highest level
+                        of gameplay. With optimal visibility from the inside,
+                        spectators can enjoy an unobstructed view of the playing
+                        surface. Ideal for both outdoor and indoor
+                        installations, it serves as a perfect Centre Court
+                        within any Padel environment. As always, we provide a
+                        seamless turnkey project experience, from supply to
+                        construction.
+                      </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-3">
+                      <AccordionTrigger
+                        onClick={() => setActiveItem("item-3")}
+                        onClose={() => setActiveItem(null)}
                       >
-                        Customised Solutions
-                      </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      Our team of experts provides customised solutions to cater
-                      to the specific needs of our clients.
-                    </AccordionContent>
-                  </AccordionItem>
-                  <AccordionItem value="item-4">
-                    <AccordionTrigger
-                      onClick={() => setActiveItem("item-4")}
-                      onClose={() => setActiveItem(null)}
-                    >
-                      <div
-                        className={`font-bold ${
-                          activeItem === "item-4"
-                            ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
-                            : "text-black"
-                        }`}
+                        <div
+                          className={`font-bold ${
+                            activeItem === "item-3"
+                              ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
+                              : "text-black"
+                          }`}
+                        >
+                          Panoramic
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        Experience the epitome of professional Padel with our
+                        spectacularly designed court, crafted to elevate your
+                        game to the highest level. Enjoy an unobstructed view of
+                        the playing surface, providing viewers with an optimal
+                        inside perspective. This court is ideally suited for
+                        indoor installations, but it can also be set up outdoors
+                        in wind-protected areas. As always, we offer a seamless
+                        turnkey project, handling everything from supply to
+                        construction. Get ready to immerse yourself in the
+                        ultimate Padel experience with our exceptional court
+                        design.
+                      </AccordionContent>
+                    </AccordionItem>
+                    <AccordionItem value="item-4">
+                      <AccordionTrigger
+                        onClick={() => setActiveItem("item-4")}
+                        onClose={() => setActiveItem(null)}
                       >
-                        I am not sure what Padel is
-                      </div>
-                    </AccordionTrigger>
-                    <AccordionContent>
-                      <button
-                        onClick={handleAbout}
-                        className="mt-4 bg-gradient-to-r from-black to-[#144a9c] text-white px-4 py-2 font-bold rounded hover:bg-gradient-to-r hover:from-[#144a9c] hover:to-black hover:scale-125 hover:transition-all transition-all hover:duration-1000 duration-1000"
-                      >
-                        Click Here
-                      </button>
-                    </AccordionContent>
-                  </AccordionItem>
-                </Accordion>
+                        <div
+                          className={`font-bold ${
+                            activeItem === "item-4"
+                              ? "bg-gradient-to-l from-[#144a9c] to-black bg-clip-text text-transparent"
+                              : "text-black"
+                          }`}
+                        >
+                          What is my ROI?
+                        </div>
+                      </AccordionTrigger>
+                      <AccordionContent>
+                        <button
+                          onClick={handleAbout}
+                          className="mt-4 bg-gradient-to-r from-black to-[#144a9c] text-white px-4 py-2 font-bold rounded hover:bg-gradient-to-r hover:from-[#144a9c] hover:to-black hover:transition-all transition-all hover:duration-1000 duration-1000"
+                        >
+                          Click Here
+                        </button>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </Accordion>
+                </div>
+                <Player
+                  src="/padelconstruction.mp4"
+                  autoPlay
+                  muted
+                  playsInline
+                  fluid={true}
+                />
               </div>
             </div>
           </div>
